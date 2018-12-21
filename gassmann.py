aea6b33f007f4c49d3c1f218417628c63e64f014
geomaar <40039638+geomaar@users.noreply.github.com>
2018-12-21 15:32:33 +0100

    Fluid substitution Gassmann
    Code for Gassmann and inverse Gassmann

---
+++ gassmann.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def gassmann(K_0, K_dry, K_fl, por):
+    """Calculation of Gassmann equation.
+    
+    Args:
+        K_0: Mineral bulk modulus (scalar)
+        K_dry: Dry rock frame (skeleton) bulk modulus (vector)
+        K_fl: Fluid bulk modulus (scalar)
+        por: Porosity (vector)
+
+    Returns: 
+        K_sat: Saturated rock bulk modulus.
+    """
+    a = (1- np.divide(K_dry, K_0))**2
+    b = np.divide(por,K_fl) + np.divide(1-por,K_0) - np.divide(K_dry,K_0**2)
+    if b==0:
+        K_sat = K_dry
+    else:
+        K_sat = K_dry + np.divide(a,b)
+        
+    return K_sat
+
+def gassmanninv(K_0,K_sat,K_fl,por):
+    """ Calculation of inverse Gassmann equation.
+    
+    Args:
+        K_0: Mineral bulk modulus (scalar)
+        K_sat: Saturated rock bulk modulus (vector)
+        K_fl: Fluid bulk modulus (scalar)
+        por: Porosity (vector)
+    
+    Returns:
+        K_dry: Dry rock frame (skeleton) bulk modulus (vector)
+    """
+
+    a = ( por*K_0)/ K_fl  + 1 - por
+    b = ( por*K_0)/ K_fl  + K_sat/K_0 - 1 - por
+    K_dry = np.divide(K_sat*a-K_0,b )
+    return K_dry
